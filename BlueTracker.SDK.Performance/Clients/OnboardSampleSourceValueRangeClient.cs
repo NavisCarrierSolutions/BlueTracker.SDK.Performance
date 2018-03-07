c8a26e76d1f40555f2068aa78277d9ac235e8b1c
Tim Petersen <tim.petersen@interschalt.de>
2018-03-07 09:38:14 +0100

    add sampleSourceValueRangeClient

---
+++ BlueTracker.SDK.Performance/Clients/OnboardSampleSourceValueRangeClient.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using BlueTracker.SDK.Performance.Core;
+using BlueTracker.SDK.Performance.Query;
+
+namespace BlueTracker.SDK.Performance.Clients
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Client for getting onboard sample source value ranges.
+    /// </summary>
+    public class OnboardSampleSourceValueRangeClient : ApiWrapper
+    {
+        /// <summary>
+        /// Creates a new OnboardSampleSourceValueRangeClient instance.
+        /// </summary>
+        /// <param name="serverAddress">The server address.</param>
+        /// <param name="authorization">The API token.</param>
+        /// <remarks>
+        /// Instead of specifying the server address and the API token with constructur parameters,
+        /// they can be set in the appSettings section of the app.config. The key BlueCloud_ApiKey
+        /// is used to specify the API token, the key BlueCloud_ServerAddress is used to set the
+        /// service address. If the service address is neither specified as constructor parameter,
+        /// nor in the app settings, the default service address will be used.
+        /// </remarks>
+        public OnboardSampleSourceValueRangeClient(string serverAddress = null, string authorization = null) :
+            base(serverAddress, authorization)
+        {
+        }
+
+        /// <summary>
+        /// Gets all onboard sample sources value ranges for a specific source
+        /// </summary>
+        /// <param name="sourceName">Name of sample source</param>
+        /// <returns>List of all sample sources value ranges</returns>
+        public List<OnboardSampleSourceValueRange> Get(string sourceName)
+        {
+            return GetObject<List<OnboardSampleSourceValueRange>>($"/api/v1/onboardSamples/sources/{sourceName}/valueRanges");
+        }
+
+        /// <summary>
+        /// Get a specific sample source identified by name
+        /// </summary>
+        /// <param name="sourceId">The ID of the sample source</param>
+        /// <returns>List of all sample sources value ranges</returns>
+        public List<OnboardSampleSourceValueRange> Get(int sourceId)
+        {
+            return GetObject<List<OnboardSampleSourceValueRange>>($"/api/v1/onboardSamples/sources/{sourceId}/valueRanges");
+        }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Query/OnboardSampleSourceValueRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTracker.SDK.Performance.Query
+{
+    public class OnboardSampleSourceValueRange
+    {
+        public int ImoNumber { get; set; }
+
+        public DateTime MinimumTimeStamp { get; set; }
+
+        public DateTime MaximumTimeStamp { get; set; }
+    }
+}
