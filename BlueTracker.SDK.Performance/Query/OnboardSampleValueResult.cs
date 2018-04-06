7db5cd821bad20142986112453c77c22d1dcc743
Tim Petersen <tim.petersen@interschalt.de>
2018-04-06 11:51:33 +0200

    XML comments

--- BlueTracker.SDK.Performance/Query/OnboardSampleValueResult.cs
+++ BlueTracker.SDK.Performance/Query/OnboardSampleValueResult.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace BlueTracker.SDK.Performance.Query
 {
+    /// <summary>
+    /// OnboardSampleValueResult is the return value for POSTing onboard sample values.
+    /// </summary>
     public class OnboardSampleValueResult
     {
+        /// <summary>
+        /// Count of inserted items (into database)
+        /// </summary>
         public int InsertCount { get; set; }
+        /// <summary>
+        /// Elapsed milliseconds storing onboard sample values
+        /// </summary>
         public long? ElapsedMillisecondsStoringOnboardSampleValue { get; set; }
+        /// <summary>
+        /// Elapsed milliseconds storing nav sample values
+        /// </summary>
         public long? ElapsedMillisecondsStoringNavSample { get; set; }
     }
 }
