4d9d0d5e032e6aa52d9e8d557b5fea0403a935af
Tim Petersen <tim.petersen@interschalt.de>
2018-05-14 11:33:48 +0200

    added consumption curve client

---
+++ BlueTracker.SDK.Performance/Post/ConsumptionCurve.cs
@@ -0,0 +1,47 @@
+﻿using BlueTracker.SDK.Performance.Enums;
+using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.Post
+{
+    /// <summary>
+    /// The consumption curve
+    /// </summary>
+    public class ConsumptionCurve
+    {
+        /// <summary>
+        /// The Id of the charter curve
+        /// </summary>
+        [JsonProperty("charterCurveId")]
+        public int CharterCurveId { get; set; }
+
+        /// <summary>
+        /// The name of the consumption curve
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the consumption curve
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The definition of the consumption curve
+        /// </summary>
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+
+        /// <summary>
+        /// The type of consumption curve
+        /// </summary>
+        [JsonProperty("consumptionCurveType")]
+        public ConsumptionCurveType ConsumptionCurveType { get; set; }
+
+        /// <summary>
+        /// The reason of change
+        /// </summary>
+        [JsonProperty("changeReason")]
+        public string ChangeReason { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Query/ConsumptionCurve.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using BlueTracker.SDK.Performance.Enums;
+using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.Query
+{
+    /// <summary>
+    /// Represents a consumption curve
+    /// </summary>
+    public class ConsumptionCurve
+    {
+        /// <summary>
+        /// Id of consumption curve
+        /// </summary>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Id of charter curve
+        /// </summary>
+        [JsonProperty("charterCurveId")]
+        public int CharterCurveId { get; set; }
+
+        /// <summary>
+        /// Name of consumption curve
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of consumption curve
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Definition of consumption curve
+        /// </summary>
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+
+        /// <summary>
+        /// Reason of change
+        /// </summary>
+        [JsonProperty("changeReason")]
+        public string ChangeReason { get; set; }
+
+        /// <summary>
+        /// Ship id created from
+        /// </summary>
+        [JsonProperty("createdFromShipId")]
+        public int? CreatedFromShipId { get; set; }
+
+        /// <summary>
+        /// Created from start
+        /// </summary>
+        [JsonProperty("createdFromDateStart")]
+        public DateTime? CreatedFromDateStart { get; set; }
+
+        /// <summary>
+        /// Created from end
+        /// </summary>
+        [JsonProperty("createdFromDateEnd")]
+        public DateTime? CreatedFromDateEnd { get; set; }
+
+        /// <summary>
+        /// The revision
+        /// </summary>
+        [JsonProperty("revision")]
+        public int Revision { get; set; }
+
+        /// <summary>
+        /// Time created
+        /// </summary>
+        [JsonProperty("createdOn")]
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Type of consumption curve
+        /// </summary>
+        [JsonProperty("consumptionCurveType")]
+        public ConsumptionCurveType ConsumptionCurveType { get; set; }
+    }
+}
