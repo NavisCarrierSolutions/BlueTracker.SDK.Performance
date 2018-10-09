2e9b48d37d3680f25dde82cb0990091f159c0d91
Carsten Krueger <carsten.krueger@ncs-devops.com>
2018-10-09 08:36:58 +0200

    Add bunker and gainLoss to performancereport model

---
+++ BlueTracker.SDK.Performance/Model/Basic/Report/Bunker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.Model.Basic.Report
+{
+    public class Bunker
+    {
+        /// <summary>
+        /// List of fuel oil quantities bunkered.
+        /// </summary>
+        [JsonProperty(PropertyName = "fuelOil")]
+        public List<BunkerFuel> FuelOil { get; set; }
+
+        /// <summary>
+        /// List of lub oil quantities bunkered.
+        /// </summary>
+        [JsonProperty(PropertyName = "lubOil")]
+        public List<BunkerLubOil> LubOil { get; set; }
+
+        /// <summary>
+        /// List of fresh water quantities bunkered.
+        /// </summary>
+        [JsonProperty(PropertyName = "freshWater")]
+        public List<FreshWaterQuantity> FreshWater { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Model/Basic/Report/GainLoss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.Model.Basic.Report
+{
+    public class GainLoss
+    {
+        /// <summary>
+        /// List of fuel oil quantities gained or lost.
+        /// </summary>
+        [JsonProperty(PropertyName = "fuelOil")]
+        public List<FuelQuantity> FuelOil { get; set; }
+
+        /// <summary>
+        /// List of lub oil quantities gained or lost.
+        /// </summary>
+        [JsonProperty(PropertyName = "lubOil")]
+        public List<LubOilQuantity> LubOil { get; set; }
+
+        /// <summary>
+        /// List of fresh water quantities gained or lost.
+        /// </summary>
+        [JsonProperty(PropertyName = "freshWater")]
+        public List<FreshWaterQuantity> FreshWater { get; set; }
+    }
+}
