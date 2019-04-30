7f8f0ea437798392cc62331f4330d36763184fa9
Carsten Krueger <carsten.krueger@ncs-devops.com>
2019-04-30 14:50:43 +0200

    Add client for nav sample values

---
+++ BlueTracker.SDK.Performance/Clients/NavSampleValueClient.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using BlueTracker.SDK.Performance.Core;
+using BlueTracker.SDK.Performance.DTO.Post;
+using BlueTracker.SDK.Performance.DTO.Query;
+
+namespace BlueTracker.SDK.Performance.Clients
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Client for getting and creating nav samples.
+    /// </summary>
+    public class NavSampleValueClient : ApiWrapper
+    {
+        /// <summary>
+        /// Creates a new <see cref="NavSampleValueClient"/> instance.
+        /// </summary>
+        /// <param name="authorization">The API token.</param>
+        public NavSampleValueClient(string authorization) : base(authorization)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="NavSampleValueClient"/> instance.
+        /// </summary>
+        /// <param name="serverAddress"></param>
+        /// <param name="authorization"></param>
+        public NavSampleValueClient(string serverAddress, string authorization) : base(serverAddress, authorization)
+        {
+        }
+
+        /// <summary>
+        /// Gets the nav sample for the specified source, imo and report.
+        /// </summary>
+        /// <param name="sourceName">Name of the nav sample source.</param>
+        /// <param name="imoNumber">IMO number of ship to get nav sample for.</param>
+        /// <param name="customId">Custom ID.</param>
+        /// <returns>The nav sample object.</returns>
+        public NavSample GetSpecific(string sourceName, int imoNumber, int customId)
+        {
+            var reqString = $"/api/v1/ships/{imoNumber}/navSamples/{sourceName}/{customId}";
+            var ret = GetObject<NavSample>(reqString);
+            return ret;
+        }
+
+        /// <summary>
+        ///  Gets a <see cref="DateLimitedSearchResult{T}"/> list of <see cref="NavSample"/>s
+        /// filtered by a time range for the nav sample source with the specified ID.
+        /// </summary>
+        /// <param name="sourceId">ID of the nav sample source.</param>
+        /// <param name="imoNumber">IMO number of the ship.</param>
+        /// <param name="start">Start date and time for the query.</param>
+        /// <param name="end">End date and time for the query.</param>
+        /// <param name="page">The page number of the query. (Optional. Default: 0)</param>
+        /// <param name="pageSize">The page size of the query. (Optional. Default: 100)</param>
+        /// <returns></returns>
+        public DateLimitedSearchResult<NavSample> GetAllBySourceId(int sourceId, int imoNumber,
+            DateTime start, DateTime end, int page = 0, int pageSize = 100)
+        {
+            var reqString = $"/api/v1/ships/{imoNumber}/navSamples/{sourceId}?start={start:yyyy-MM-ddTHH:mm}&end={end:yyyy-MM-ddTHH:mm}&page={page}&pageSize={pageSize}";
+
+            var ret = GetObject<DateLimitedSearchResult<NavSample>>(reqString);
+            return ret;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="DateLimitedSearchResult{T}"/> list of <see cref="NavSample"/>s
+        /// filtered by a time range for the nav sample source with the specified name.
+        /// </summary>
+        /// <param name="sourceName">Name of the nav sample source.</param>
+        /// <param name="imoNumber">IMO number of the ship.</param>
+        /// <param name="start">Start date and time for the query.</param>
+        /// <param name="end">End date and time for the query.</param>
+        /// <param name="page">The page number of the query. (Optional. Default: 0)</param>
+        /// <param name="pageSize">The page size of the query. (Optional. Default: 100)</param>
+        /// <returns></returns>
+        public DateLimitedSearchResult<NavSample> GetAllBySourceName(string sourceName, int imoNumber,
+            DateTime start, DateTime end, int page = 0, int pageSize = 100)
+        {
+            var reqString = $"/api/v1/ships/{imoNumber}/navSamples/{sourceName}?start={start:yyyy-MM-ddTHH:mm}&end={end:yyyy-MM-ddTHH:mm}&page={page}&pageSize={pageSize}";
+
+            var ret = GetObject<DateLimitedSearchResult<NavSample>>(reqString);
+            return ret;
+        }
+
+        /// <summary>
+        /// Creates new nav samples.
+        /// </summary>
+        /// <param name="sourceName">Name of source to create nav samples for.</param>
+        /// <param name="imoNumber">IMO of ship to create nav sample for.</param>
+        /// <param name="navSampleData">List of nav samples to create.</param>
+        /// <returns></returns>
+        public int Create(string sourceName, int imoNumber, List<NavSampleData> navSampleData)
+        {
+            return PostObject<int, List<NavSampleData>>(navSampleData, $"/api/v1/ships/{imoNumber}/navSamples/{sourceName}");
+        }
+    }
+}
---
+++ BlueTracker.SDK.Performance/DTO/Post/NavSampleData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.DTO.Post
+{
+    /// <summary>
+    /// Navigational sample data
+    /// </summary>
+    public class NavSampleData
+    {
+        /// <summary>
+        /// UTC timestamp.
+        /// </summary>
+        [JsonProperty("timeStampUtc")]
+        public DateTime TimeStampUtc { get; set; }
+
+        /// <summary>
+        /// Custom ID.
+        /// </summary>
+        [JsonProperty("customId")]
+        public int? CustomId { get; set; }
+
+        /// <summary>
+        /// Latitude [deg].
+        /// </summary>
+        [JsonProperty("lat")]
+        public double? Lat { get; set; }
+
+        /// <summary>
+        /// Longitude [deg].
+        /// </summary>
+        [JsonProperty("lng")]
+        public double? Lng { get; set; }
+
+        /// <summary>
+        /// Heading [deg]
+        /// </summary>
+        [JsonProperty("hdg")]
+        public double? Hdg { get; set; }
+
+        /// <summary>
+        /// Course over ground [deg].
+        /// </summary>
+        [JsonProperty("cog")]
+        public double? Cog { get; set; }
+
+        /// <summary>
+        /// Speed over ground [kn].
+        /// </summary>
+        [JsonProperty("sog")]
+        public double? Sog { get; set; }
+
+        /// <summary>
+        /// Speed through water [kn].
+        /// </summary>
+        [JsonProperty("stw")]
+        public double? Stw { get; set; }
+    }
+}
