c7335483d4ff26195053bf0d8432f4cacb02f584
Carsten Krueger <carsten.krueger@ncs-devops.com>
2019-05-03 08:36:14 +0200

    Let SDK user set whether ship is on shore power supply

--- BlueTracker.SDK.Performance/Model/Processing/Report/Engine.cs
+++ BlueTracker.SDK.Performance/Model/Processing/Report/Engine.cs
@@ -15,6 +15,8 @@ namespace BlueTracker.SDK.Performance.Model.Processing.Report
 
         public bool? IsCombinatorMode { get; set; }
 
+        public bool? IsOnShorePowerSupply { get; set; }
+
         public double? TotalFoc { get; set; }
 
         public double? TotalFocIso { get; set; }
