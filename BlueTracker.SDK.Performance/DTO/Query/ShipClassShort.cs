27aca91679c4856e20f1224eba7f2c13fe548d33
Henrik Alfke <argonavis80@users.noreply.github.com>
2020-05-08 13:41:38 +0200

    Add ship client and additional DTOs (#62)
    * Add ship client and additional DTOs; Fixed some typos; Added missing properties.
    
    * Add missing properties for class, owner and port.
    
    * Removed SetBatchMode and Calculate method from ReportClient.

---
+++ BlueTracker.SDK.Performance/DTO/Query/ShipClassShort.cs
@@ -0,0 +1,23 @@
+﻿namespace BlueTracker.SDK.Performance.DTO.Query
+{
+    /// <summary>
+    /// A ship class.
+    /// </summary>
+    public class ShipClassShort
+    {
+        /// <summary>
+        /// ID of ship class.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// CustomId of ship class.
+        /// </summary>
+        public string CustomId { get; set; }
+
+        /// <summary>
+        /// Name of ship class.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
