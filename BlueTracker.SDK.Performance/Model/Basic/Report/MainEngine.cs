a275b9dad0ae28162b498cb33f1378e72c465a22
Carsten Krueger <carsten.krueger@ncs-devops.com>
2020-05-19 07:29:00 +0200

    Let user report lub oil consumptions per engine

--- BlueTracker.SDK.Performance/Model/Basic/Report/MainEngine.cs
+++ BlueTracker.SDK.Performance/Model/Basic/Report/MainEngine.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace BlueTracker.SDK.Performance.Model.Basic.Report
 {
@@ -54,5 +55,11 @@ namespace BlueTracker.SDK.Performance.Model.Basic.Report
         /// </summary>
         [JsonProperty(PropertyName = "propellerPitch")]
         public double? PropellerPitch { get; set; }
+
+        /// <summary>
+        /// Engine specific list of lub oil quantities consumed.
+        /// </summary>
+        [JsonProperty(PropertyName = "lubOilConsumptions")]
+        public List<LubOilConsumption> LubOilConsumptions { get; set; }
     }
 }
--- BlueTracker.SDK.Performance/Model/Basic/Report/OtherConsumption.cs
+++ BlueTracker.SDK.Performance/Model/Basic/Report/OtherConsumption.cs
@@ -4,15 +4,15 @@ using Newtonsoft.Json;
 namespace BlueTracker.SDK.Performance.Model.Basic.Report
 {
     /// <summary>
-    /// Information about consumptions other than fueloil.
+    /// Information about consumptions other than fuel oil.
     /// </summary>
     public class OtherConsumption
     {
         /// <summary>
-        /// List of luboil volumes consumed.
+        /// List of lub oil volumes consumed.
         /// </summary>
         [JsonProperty(PropertyName = "lubOil")]
-        public List<LubOilConsumption> LubOil { get; set; }
+        public List<LubOilAggregateConsumption> LubOil { get; set; }
 
         /// <summary>
         /// List of fresh water quantities consumed.
