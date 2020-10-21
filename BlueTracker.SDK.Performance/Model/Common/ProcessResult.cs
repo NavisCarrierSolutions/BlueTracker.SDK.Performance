ac12e896c9ec86c9888d9398eeb554cfaddaa492
jonathan.hotting <jonathan.hotting@ncs-devops.com>
2020-10-21 09:56:02 +0200

    added missing feedback models

---
+++ BlueTracker.SDK.Performance/Model/Common/ProcessResult.cs
@@ -0,0 +1,15 @@
+﻿using BlueTracker.SDK.Performance.Model.Enums;
+
+namespace BlueTracker.SDK.Performance.Model.Common
+{
+    public class ProcessResult
+    {
+        public ProcessResultOptions Result { get; set; }
+
+        public string Message { get; set; }
+
+        public double Accuracy { get; set; }
+
+        public double? FinalAccuracy { get; set; }
+    }
+}
