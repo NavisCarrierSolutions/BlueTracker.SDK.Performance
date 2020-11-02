f81a4cbd236fce1e93a470a361598abe515c11cb
bonaparte89 <jonathan.hotting@navis.com>
2020-11-02 10:15:18 +0100

    Added feedback options (#82)
    * added missing feedback models
    
    * Add ncvs-cloud as publish destination
    
    Co-authored-by: jonathan.hotting <jonathan.hotting@ncs-devops.com>
    Co-authored-by: Philipp Ranft <philipp.ranft@navis.com>

---
+++ BlueTracker.SDK.Performance/Model/Enums/PlausibilityResultOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    public enum PlausibilityResultOptions
+    {
+        NotSet = 0,
+
+        Invalid = 1,
+
+        Fine = 2,
+
+        Minor = 3,
+
+        Major = 4
+    }
+}
