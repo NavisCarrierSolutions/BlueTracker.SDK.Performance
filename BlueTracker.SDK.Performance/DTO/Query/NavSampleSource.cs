180cc66c53bd50baebe64eb111422b1ac7264685
Tim Petersen <tim.petersen@interschalt.de>
2021-01-11 10:55:49 +0100

    NavSampleSource client

---
+++ BlueTracker.SDK.Performance/DTO/Query/NavSampleSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueTracker.SDK.Performance.Model.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace BlueTracker.SDK.Performance.DTO.Query
+{
+    public class NavSampleSource
+    {
+        public int Id { get; set; }
+
+        public string Key { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool SystemReserved { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public NavSampleSourceType SourceType { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public SampleVisibility Visibility { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Model/Enums/NavSampleSourceType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    public enum NavSampleSourceType
+    {
+        Unspecified = 0,
+
+        Reported = 1,
+
+        Measured = 2,
+
+        Polled = 3,
+
+        Ais = 4
+    }
+}
