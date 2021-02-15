b34a50867668bda5a7ad6a1ddede9cfa15805bb3
Carsten Krueger <carsten.krueger@ncs-devops.com>
2021-02-15 11:07:11 +0100

    Add waste disposal to SDK

---
+++ BlueTracker.SDK.Performance/Model/Enums/WasteMainType.cs
@@ -0,0 +1,73 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    /// <summary>
+    /// States the different waste disposal main categories.
+    /// </summary>
+    public enum WasteMainType
+    {
+        /// <summary>
+        /// Unspecified.
+        /// </summary>
+        Unspecified = 0,
+
+        /// <summary>
+        /// Category A: Plastic.
+        /// </summary>
+        CategoryAPlastic = 1,
+
+        /// <summary>
+        /// Category B: Food waste.
+        /// </summary>
+        CategoryBFoodWaste = 2,
+
+        /// <summary>
+        /// Category C: Domestic waste.
+        /// </summary>
+        CategoryCDomesticWaste = 3,
+
+        /// <summary>
+        /// Category D: Cooking oil.
+        /// </summary>
+        CategoryDCookingOil = 4,
+
+        /// <summary>
+        /// Category E: Incinerator ash.
+        /// </summary>
+        CategoryEIncineratorAsh = 5,
+
+        /// <summary>
+        /// Category F: Operational waste.
+        /// </summary>
+        CategoryFOperationalWaste = 6,
+
+        /// <summary>
+        /// Category G: Animal carcass.
+        /// </summary>
+        CategoryGAnimalCarcass = 7,
+
+        /// <summary>
+        /// Category H: Fishing gear.
+        /// </summary>
+        CategoryHFishingGear = 8,
+
+        /// <summary>
+        /// Category I: Electronic waste.
+        /// </summary>
+        CategoryIElectronicWaste = 9,
+
+        /// <summary>
+        /// Category J: Cargo residue (non HME).
+        /// </summary>
+        CategoryJCargoResidueNonHme = 10,
+
+        /// <summary>
+        /// Category K: Cargo residue (HME).
+        /// </summary>
+        CategoryKCargoResidueHme = 11,
+
+        /// <summary>
+        /// Sludge and oil residue.
+        /// </summary>
+        SludgeAndOilResidue = 12
+    }
+}
