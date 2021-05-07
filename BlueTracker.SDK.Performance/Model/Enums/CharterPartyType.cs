72126403afc9cd6a0ed33d4c62e59e448d3646dc
thegoodsoldier <carsten.2.krueger@navis.com>
2021-05-07 10:42:44 +0200

    Add weather data source to charter party def (#87)
    Co-authored-by: Carsten Krueger <carsten.krueger@ncs-devops.com>
    Co-authored-by: Tim Petersen <TimJuPe@gmail.com>

--- BlueTracker.SDK.Performance/Model/Enums/CharterPartyType.cs
+++ BlueTracker.SDK.Performance/Model/Enums/CharterPartyType.cs
@@ -2,8 +2,19 @@
 {
     public enum CharterPartyType
     {
+        /// <summary>
+        /// Unspecified
+        /// </summary>
         Unspecified = 0,
+
+        /// <summary>
+        /// Charter party is based on consumption curve
+        /// </summary>
         ConsumptionCurve = 1,
+
+        /// <summary>
+        /// Charter party is based on consumption cap
+        /// </summary>
         ConsumptionCap = 2
     }
 }
---
+++ BlueTracker.SDK.Performance/Model/Enums/WeatherSourceOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    public enum WeatherSourceOptions
+    {
+        /// <summary>
+        /// Weather source is not specified.
+        /// </summary>
+        Unspecified = 0,
+
+        /// <summary>
+        /// Weather information comes from vessel reports.
+        /// </summary>
+        VesselReports = 1,
+
+        /// <summary>
+        /// Weather information comes from weather provider.
+        /// </summary>
+        WeatherProvider = 2
+    }
+}
