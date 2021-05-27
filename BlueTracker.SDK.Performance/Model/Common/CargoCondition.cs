f916dc0a5fef02f81b1d4a18cb577f77f617a689
thegoodsoldier <carsten.2.krueger@navis.com>
2021-05-27 07:25:32 +0200

    Add charter party instructions (#93)
    Co-authored-by: Carsten Krueger <carsten.krueger@ncs-devops.com>

---
+++ BlueTracker.SDK.Performance/Model/Common/CargoCondition.cs
@@ -0,0 +1,44 @@
+﻿using BlueTracker.SDK.Performance.Model.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace BlueTracker.SDK.Performance.Model.Common
+{
+    public class CargoCondition
+    {
+        /// <summary>
+        /// Gets or sets the maximum allowed daily consumption [t/d]
+        /// (Used only for charter party type ConsumptionCap).
+        /// </summary>
+        [JsonProperty("consumptionCap")]
+        public double? ConsumptionCap { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum allowed speed [kn]
+        /// (Used only for charter party type ConsumptionCap).
+        /// </summary>
+        [JsonProperty("speedCap")]
+        public double? SpeedCap { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum required speed [kn]
+        /// (Used only for charter party type ConsumptionCap).
+        /// </summary>
+        [JsonProperty("minimumSpeed")]
+        public double? MinimumSpeed { get; set; }
+
+        /// <summary>
+        /// Speed instruction of cargo condition.
+        /// </summary>
+        [JsonProperty(PropertyName = "charterSpeedInstruction")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CharterSpeedInstruction CharterSpeedInstruction { get; set; }
+
+        /// <summary>
+        /// Voyage status of cargo condition.
+        /// </summary>
+        [JsonProperty(PropertyName = "charterVoyageStatus")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CharterVoyageStatus CharterVoyageStatus { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Model/Enums/CharterSpeedInstruction.cs
@@ -0,0 +1,20 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    public enum CharterSpeedInstruction
+    {
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        Undefined = 0,
+
+        /// <summary>
+        /// Eco
+        /// </summary>
+        Eco = 1,
+
+        /// <summary>
+        /// Full
+        /// </summary>
+        Full = 2
+    }
+}
