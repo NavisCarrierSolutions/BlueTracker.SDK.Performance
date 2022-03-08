ce6f569c56188d0fb04c0358312e5e8ac1bf20a4
Tim Petersen <Tim.Petersen@navis.com>
2022-03-08 14:41:11 +0100

    create client methods for voyageClient -> parcel sequence post and get

---
+++ BlueTracker.SDK.Performance/DTO/Post/CargoParcelSequenceData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTracker.SDK.Performance.DTO.Post
+{
+    /// <summary>
+    /// Defines a cargo parcel sequence for voyage.
+    /// </summary>
+    public class CargoParcelSequenceData
+    {
+        public List<CargoParcelData> CargoParcels { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/DTO/Query/CargoParcelSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTracker.SDK.Performance.DTO.Query
+{
+    /// <summary>
+    /// Describes a sequence of cargo movements.
+    /// </summary>
+    public class CargoParcelSequence
+    {
+        /// <summary>
+        /// Unique identifier of voyage.
+        /// </summary>
+        public int VoyageId { get; set; }
+
+        /// <summary>
+        /// Name of voyage.
+        /// </summary>
+        public string VoyageName { get; set; }
+
+        /// <summary>
+        /// IMO number of vessel.
+        /// </summary>
+        public int ImoNumber { get; set; }
+
+        /// <summary>
+        /// Name of vessel.
+        /// </summary>
+        public string VesselName { get; set; }
+
+        /// <summary>
+        /// Collection of cargo parcels.
+        /// </summary>
+        public List<CargoParcel> CargoParcels { get; set; }
+    }
+}
