b29234f2b2b31ac4f46bba329446547ba96c8c34
thegoodsoldier <carsten.2.krueger@navis.com>
2022-11-17 14:46:17 +0100

    Add auxiliary systems to report DTO (#105)
    * Add auxiliary systems to report DTO
    
    * Change units
    
    * Streamline aux systems with sample DTOs
    
    * Rename to "CoolingSystems"
    
    * Change pumps
    
    Co-authored-by: Tim Petersen <TimJuPe@gmail.com>

---
+++ BlueTracker.SDK.Performance/Model/Basic/Report/CoolingSystem.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace BlueTracker.SDK.Performance.Model.Basic.Report
+{
+    /// <summary>
+    /// Represents a cooling system
+    /// </summary>
+    public class CoolingSystem : EnergyConsumer
+    {
+        /// <summary>
+        /// Name of the cooling system.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
