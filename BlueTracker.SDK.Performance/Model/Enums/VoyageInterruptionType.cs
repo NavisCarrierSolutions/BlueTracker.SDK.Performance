770da975968f3806fb0e815669f9416cb8ca5ffb
thegoodsoldier <carsten.2.krueger@navis.com>
2023-01-23 08:10:14 +0100

    Add voyage interruptions client (#114)
    * Add voyage interruption client
    
    * Update readme

---
+++ BlueTracker.SDK.Performance/Model/Enums/VoyageInterruptionType.cs
@@ -0,0 +1,50 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    public enum VoyageInterruptionType
+    {
+        /// <summary>
+        /// Unspecified
+        /// </summary>
+        Unspecified = 0,
+
+        /// <summary>
+        /// Layup start
+        /// </summary>
+        LayupStart = 1,
+
+        /// <summary>
+        /// Layup end
+        /// </summary>
+        LayupEnd = 2,
+
+        /// <summary>
+        /// Drydock start
+        /// </summary>
+        DryDockStart = 3,
+
+        /// <summary>
+        /// Drydock end
+        /// </summary>
+        DryDockEnd = 4,
+
+        /// <summary>
+        /// Emergency start
+        /// </summary>
+        EmergencyStart = 5,
+
+        /// <summary>
+        /// Emergency end
+        /// </summary>
+        EmergencyEnd = 6,
+
+        /// <summary>
+        /// Ice start
+        /// </summary>
+        IceStart = 7,
+
+        /// <summary>
+        /// Ice end
+        /// </summary>
+        IceEnd = 8
+    }
+}
