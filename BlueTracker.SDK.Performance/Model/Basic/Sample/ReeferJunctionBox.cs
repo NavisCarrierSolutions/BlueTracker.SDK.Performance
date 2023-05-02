2356a36d8b3ca00a08d80f1005f086a9cd790373
Tim Petersen <Tim.Petersen@navis.com>
2023-05-02 12:23:46 +0200

    reefer boxes for deck equipment sample

---
+++ BlueTracker.SDK.Performance/Model/Basic/Sample/ReeferJunctionBox.cs
@@ -0,0 +1,18 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Basic.Sample
+{
+    public class ReeferJunctionBox : EnergyConsumer
+    {
+        /// <summary>
+        ///     Winding Temperature T
+        /// </summary>
+        public double? WindingTemperatureT { get; set; }
+        /// <summary>
+        ///     Winding Temperature R
+        /// </summary>
+        public double? WindingTemperatureR { get; set; }
+        /// <summary>
+        ///     Winding Temperature S
+        /// </summary>
+        public double? WindingTemperatureS { get; set; }
+    }
+}
