3345b9d47052624367f80c7ae55ca60bc5c7df5e
Carsten Krüger <carsten.krueger@kaleris.com>
2024-07-16 09:34:10 +0200

    Add issue client and corresponding DTOs (#145)
    * Add issue client and corresponding DTOs
    
    * Adjust PerformanceReportIssueItem DTO
    
    * Align parameter names with the ones in the API
    
    * Update README

---
+++ BlueTracker.SDK.Performance/DTO/Query/PerformanceReportIssueItem.cs
@@ -0,0 +1,67 @@
+﻿using BlueTracker.SDK.Performance.Model.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace BlueTracker.SDK.Performance.DTO.Query
+{
+    /// <summary>
+    /// Provides information about a performance report issue.
+    /// </summary>
+    public class PerformanceReportIssueItem
+    {
+        /// <summary>
+        /// Local timestamp of the associated report.
+        /// </summary>
+        public DateTimeOffset TimestampLocal { get; set; }
+
+        /// <summary>
+        /// UTC timestamp of the associated report.
+        /// </summary>
+        public DateTimeOffset TimestampUtc { get; set; }
+
+        /// <summary>
+        /// Name of the issue.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value of the issue.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Message describing the issue.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Type of associated event.
+        /// </summary>
+        public EventType? Event { get; set; }
+
+        /// <summary>
+        /// State of the issue.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Plausibility state of the issue.
+        /// </summary>
+        public PlausibilityResultOptions PlausibilityResult { get; set; }
+
+        /// <summary>
+        /// Completeness state of the issue.
+        /// </summary>
+        public CompletenessResultOptions? CompletenessResult { get; set; }
+
+        /// <summary>
+        /// List of affected areas.
+        /// </summary>
+        public List<string> AffectedAreas { get; set; }
+
+        /// <summary>
+        /// List of reported values.
+        /// </summary>
+        public List<string> ReportedValues { get; set; }
+    }
+}
---
+++ BlueTracker.SDK.Performance/Model/Enums/CompletenessResultOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace BlueTracker.SDK.Performance.Model.Enums
+{
+    /// <summary>
+    /// Provides an enumeration to distinguish completeness results.
+    /// </summary>
+    public enum CompletenessResultOptions
+    {
+        /// <summary>
+        /// Indicates that some relevant data points have not been reported.
+        /// </summary>
+        Incomplete = 0,
+
+        /// <summary>
+        /// Indicates that all relevant data points have been reported.
+        /// </summary>
+        Complete = 1,
+
+        /// <summary>
+        /// Indicates that the corresponding completeness area is not licensed.
+        /// </summary>
+        NotLicensed = 2,
+
+        /// <summary>
+        /// Indicates that at the current state there are no completeness checks for the completeness area.
+        /// </summary>
+        NoChecks = 3
+    }
+}
