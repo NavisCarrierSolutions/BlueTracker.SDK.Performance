0836d3c8c06838ecd35349b5bd046aa29ffa26c0
Carsten Krueger <carsten.krueger@kaleris.com>
2026-04-08 11:51:04 +0200

    Use correct formulas in ScrubberChemicalType enum

--- BlueTracker.SDK.Performance/Model/Enums/ScrubberChemicalType.cs
+++ BlueTracker.SDK.Performance/Model/Enums/ScrubberChemicalType.cs
@@ -7,20 +7,20 @@
         /// </summary>
         Other = 0,
         /// <summary>
-        /// CA (OH)2 - Calcium hydroxide
+        /// Ca(OH)2 - Calcium hydroxide
         /// </summary>
         CaOH2 = 1,
         /// <summary>
-        /// NaCO3 - Sodium carbonate
+        /// Na2CO3 - Sodium carbonate
         /// </summary>
-        NaCO3 = 2,
+        Na2CO3 = 2,
         /// <summary>
         /// NaOH - Sodium hydroxide
         /// </summary>
         NaOH = 3,
         /// <summary>
-        /// MG(OH)2 - Magnesium hydroxide
+        /// Mg(OH)2 - Magnesium hydroxide
         /// </summary>
-        MGOH2 = 4
+        MgOH2 = 4
     }
 }
